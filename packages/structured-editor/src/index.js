3d28890a31d72db0ec07ae5e7b4548ebb5a4ba29
Jamie Holly <jamie@hollyit.net>
2019-05-24 12:59:29 -0400

    Added extensions to vue imports

--- packages/structured-editor/src/index.js
+++ packages/structured-editor/src/index.js
@@ -1,9 +1,8 @@
 export {default as SelectionEvent} from './events/SelectionEvent'
-export {default as Editable } from './components/Editable'
+export {default as Editable } from './components/Editable.vue'
 export {
     default as EditorConfiguration,
 }from './configuration/EditorConfiguration'
 
-import StructuredEditor from './components/StructuredEditor'
 
 export default StructuredEditor
